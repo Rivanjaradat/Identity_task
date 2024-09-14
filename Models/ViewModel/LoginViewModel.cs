3a888c95c87a70d103c7e4486e0294829927ba7b
Rivan jaradat <rivanr44@gmail.com>
2024-09-14 17:36:58 +0300

    first commit

---
+++ Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace identity_task.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(40)]
+        [Display (Name ="Email Address")]
+        public String Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+      
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
